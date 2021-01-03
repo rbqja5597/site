1a45632877e1285bf9144b1127fc9d222c4d6c4c
rbqja5597 <rbqja5597@naver.com>
2021-01-04 05:16:48 +0900

    1-4

--- current.sql
+++ current.sql
@@ -1,25 +1,27 @@
-/*
-SQLyog Community v13.1.7 (64 bit)
-MySQL - 10.4.14-MariaDB : Database - textBoard
-*********************************************************************
-*/
+-- MariaDB dump 10.17  Distrib 10.4.14-MariaDB, for Win64 (AMD64)
+--
+-- Host: localhost    Database: textboard
+-- ------------------------------------------------------
+-- Server version	10.4.14-MariaDB
 
-/*!40101 SET NAMES utf8 */;
-
-/*!40101 SET SQL_MODE=''*/;
-
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-CREATE DATABASE /*!32312 IF NOT EXISTS*/`textBoard` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
 
-USE `textBoard`;
-
-/*Table structure for table `article` */
+--
+-- Table structure for table `article`
+--
 
 DROP TABLE IF EXISTS `article`;
-
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `article` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `regDate` datetime NOT NULL,
@@ -30,18 +32,25 @@ CREATE TABLE `article` (
   `boardId` int(10) unsigned NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-/*Data for the table `article` */
+--
+-- Dumping data for table `article`
+--
 
-insert  into `article`(`id`,`regDate`,`updateDate`,`title`,`body`,`memberId`,`boardId`) values 
-(1,'2020-12-23 12:45:05','2020-12-23 12:45:05','공지사항','# 공지사항\r\n- 개발자 블로그\r\n```\r\n환영합니다\r\n```',3,1),
-(2,'2020-12-23 12:45:05','2020-12-23 12:45:05','자유','# 자유게시판\r\n\r\n```\r\n자유게시판입니다.\r\n```',3,2),
-(3,'2020-12-23 12:45:05','2020-12-23 12:45:05','자유','# 자유게시판\r\n- 개발자 블로그\r\n```\r\n환영합니다\r\n```',3,2);
+LOCK TABLES `article` WRITE;
+/*!40000 ALTER TABLE `article` DISABLE KEYS */;
+INSERT INTO `article` VALUES (1,'2020-12-30 12:35:40','2020-12-30 12:35:40','공지사항','# 공지사항\r\n- 개발자 블로그\r\n```\r\n환영합니다\r\n```',3,1),(2,'2020-12-30 12:35:40','2020-12-30 12:35:40','자유1','# 자유게시판\r\n\r\n```\r\n자유게시판입니다.\r\n```',3,2),(3,'2020-12-30 12:35:40','2020-12-30 12:35:40','자유2','# 자유게시판\r\n- 개발자 블로그\r\n```\r\n환영합니다.\r\n```',3,2);
+/*!40000 ALTER TABLE `article` ENABLE KEYS */;
+UNLOCK TABLES;
 
-/*Table structure for table `board` */
+--
+-- Table structure for table `board`
+--
 
 DROP TABLE IF EXISTS `board`;
-
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `board` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `regDate` datetime NOT NULL,
@@ -50,17 +59,25 @@ CREATE TABLE `board` (
   `code` char(20) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-/*Data for the table `board` */
+--
+-- Dumping data for table `board`
+--
 
-insert  into `board`(`id`,`regDate`,`updateDate`,`name`,`code`) values 
-(1,'2020-12-23 12:45:05','2020-12-23 12:45:05','공지사항','notice'),
-(2,'2020-12-23 12:45:05','2020-12-23 12:45:05','자유','free');
+LOCK TABLES `board` WRITE;
+/*!40000 ALTER TABLE `board` DISABLE KEYS */;
+INSERT INTO `board` VALUES (1,'2020-12-30 12:35:41','2020-12-30 12:35:41','공지사항','notice'),(2,'2020-12-30 12:35:41','2020-12-30 12:35:41','자유','free');
+/*!40000 ALTER TABLE `board` ENABLE KEYS */;
+UNLOCK TABLES;
 
-/*Table structure for table `member` */
+--
+-- Table structure for table `member`
+--
 
 DROP TABLE IF EXISTS `member`;
-
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `member` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `regDate` datetime NOT NULL,
@@ -70,15 +87,25 @@ CREATE TABLE `member` (
   `name` char(30) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-/*Data for the table `member` */
+--
+-- Dumping data for table `member`
+--
 
-insert  into `member`(`id`,`regDate`,`updateDate`,`loginId`,`loginPw`,`name`) values 
-(1,'2020-12-23 12:45:05','2020-12-23 12:45:05','test1','test1','테스터1'),
-(2,'2020-12-23 12:45:05','2020-12-23 12:45:05','test2','test2','테스터2'),
-(3,'2020-12-23 12:45:05','2020-12-23 12:45:05','admin','admin','김규범');
+LOCK TABLES `member` WRITE;
+/*!40000 ALTER TABLE `member` DISABLE KEYS */;
+INSERT INTO `member` VALUES (1,'2020-12-30 12:35:41','2020-12-30 12:35:41','test1','test1','테스터1'),(2,'2020-12-30 12:35:41','2020-12-30 12:35:41','test2','test2','테스터2'),(3,'2020-12-30 12:35:41','2020-12-30 12:35:41','admin','admin','김규범');
+/*!40000 ALTER TABLE `member` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
 /*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-01-04  5:13:28
